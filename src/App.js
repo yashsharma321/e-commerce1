3895fff8c9e4d11c04875cced1987be2d275760f
Yash Sharma <yash.arkedia@gmail.com>
2023-11-02 11:14:33 +0000

    folder structure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with <b>Yash</b> Sharma.
+          Learn React with Yash.
         </a>
       </header>
     </div>
