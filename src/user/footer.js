547e853d8f120bb8b2bcfb0a0e5ad51212647157
Yash Sharma <yash.arkedia@gmail.com>
2023-11-09 10:20:10 +0000

    indentation

--- src/user/footer.js
+++ src/user/footer.js
@@ -2,90 +2,90 @@
 const Footer = () =>{
     return(
         <footer className="text-center text-lg-start bg-light text-muted mt-4">
-                    <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom border-top">
-                        <div className="me-5 d-none d-lg-block">
-                            <span>Get connected with us on social networks:</span>
+            <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom border-top">
+                <div className="me-5 d-none d-lg-block">
+                    <span>Get connected with us on social networks:</span>
+                </div>
+                <div>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-google"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    <a href="#" className="me-4 text-reset">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </div>
+            </section>
+            <section>
+                <div className="container text-center text-md-start mt-5">
+                    <div className="row mt-3">
+                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">
+                                <i className="fas fa-gem me-3"></i>React Shopping App
+                            </h6>
+                            <p>
+                            This React Shopping App has two sections, one for user and another for admin.
+                            Users can browse products and place orders. Admins can register/login, add/manage inventory.
+                            </p>
                         </div>
-                        <div>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-twitter"></i>
-                            </a>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-google"></i>
-                            </a>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-instagram"></i>
-                            </a>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-linkedin"></i>
-                            </a>
-                            <a href="#" className="me-4 text-reset">
-                                <i className="fab fa-github"></i>
-                            </a>
+                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">
+                                Fastest delivery guranteed!
+                            </h6>
+                            <p>
+                                <a href="#!" className="text-reset">Bengaluru</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Pune</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Mumbai</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Noida</a>
+                            </p>
                         </div>
-                    </section>
-                    <section>
-                        <div className="container text-center text-md-start mt-5">
-                        <div className="row mt-3">
-                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    <i className="fas fa-gem me-3"></i>React Shopping App
-                                </h6>
-                                <p>
-                                This React Shopping App has two sections, one for user and another for admin.
-                                Users can browse products and place orders. Admins can register/login, add/manage inventory.
-                                </p>
-                            </div>
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    Fastest delivery guranteed!
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Bengaluru</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Pune</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Mumbai</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Noida</a>
-                                </p>
-                            </div>
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    Useful links
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Pricing</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Shipments</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Orders</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Help</a>
-                                </p>
-                            </div>
-                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                                <p><i className="fas fa-home me-3"></i>55, Marathahalli, BLR 560037, IN</p>
-                                <p><i className="fas fa-envelope me-3"></i>info@example.com</p>
-                                <p><i className="fas fa-phone me-3"></i>+ 91 86234 56788</p>
-                                <p><i className="fas fa-print me-3"></i>+ 91 97234 56789</p>
-                            </div>
+                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">
+                                Useful links
+                            </h6>
+                            <p>
+                                <a href="#!" className="text-reset">Pricing</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Shipments</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Orders</a>
+                            </p>
+                            <p>
+                                <a href="#!" className="text-reset">Help</a>
+                            </p>
                         </div>
+                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                            <p><i className="fas fa-home me-3"></i>55, Marathahalli, BLR 560037, IN</p>
+                            <p><i className="fas fa-envelope me-3"></i>info@example.com</p>
+                            <p><i className="fas fa-phone me-3"></i>+ 91 86234 56788</p>
+                            <p><i className="fas fa-print me-3"></i>+ 91 97234 56789</p>
                         </div>
-                    </section>
-                    <div className="text-center p-4 bg-info">
-                        <span>© 2023 Copyright: <a className="text-reset fw-bold" href="#">ReactShoppingApp</a></span>
                     </div>
+                </div>
+            </section>
+                <div className="text-center p-4 bg-info">
+                    <span>© 2023 Copyright: <a className="text-reset fw-bold" href="#">ReactShoppingApp</a></span>
+                </div>
         </footer>
     )
 
